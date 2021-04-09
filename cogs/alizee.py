8f206f1b88925027d39e71ab5f21a8863edadbe5
Leoš Gjumija <gjumle@protonmail.com>
2021-04-09 21:08:08 +0200

    Alizee update
    Příkaz play, join a leave

--- cogs/alizee.py
+++ cogs/alizee.py
@@ -2,16 +2,34 @@
 import discord
 from discord.ext import commands, tasks
 
-class Hello(commands.Cog):
-    """docstring forHello."""
+class Alizee(commands.Cog):
+    """docstring forAlizee."""
 
     def __init__(self, client):
         self.client = client
         self._join_remove_channel_id = 829970999788699708
 
+    players = {}
+
     ################################### Alizee
-    @commands.command()
-    
+    @commands.command(pass_context=True)
+    async def join(ctx):
+        channel = ctx.message.author.voice.voice_channel
+        await self.client.join_voice_channel(channel)
+
+    @commands.command(pass_context=True)
+    async def leave(ctx):
+        server = ctx.message.server
+        voice_client = self.client.voice_client_in(server)
+        await voice_client.disconnect(channel)
+
+    @commands.command(pass_context=True)
+    async def play(self, ctx, url):
+        server = ctx.message.server
+        voice_client = self.client.voice_client_in(server)
+        player = await voice_client.create_ytdl_player(url)
+        players[server.id] = player
+        player.start()
 
 def setup(client):
-    client.add_cog(Hello(client))
+    client.add_cog(Alizee(client))
