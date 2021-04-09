02e3db98321b240f3824d62bdf5c72ea074e0c08
Leoš Gjumija <gjumle@protonmail.com>
2021-04-09 12:00:15 +0200

    jit

--- bot.py
+++ bot.py
@@ -13,6 +13,14 @@ client = commands.Bot(command_prefix=",")
 client = discord.Client()
 
 ################################## Cog loader
+client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
--- cogs/status.py
+++ cogs/status.py
@@ -6,24 +6,24 @@ class Status(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-################################## Basic log
-@commands.Cog.listener()
-async def member_join(self, member):
-    await ctx.send(f'{member.mention} se připojil k fsociety.')
+    ################################## Basic log
+    @commands.Cog.listener()
+    async def member_join(self, member):
+        await ctx.send(f'{member.mention} se připojil k fsociety.')
 
-@commands.Cog.listener()
-async def member_remove(self, member):
-    await ctx.send(f'{member.mention} opustil fsociety.')
+    @commands.Cog.listener()
+    async def member_remove(self, member):
+        await ctx.send(f'{member.mention} opustil fsociety.')
 
-################################## Ping
-@commands.command()
-async def ping(self, ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
+    ################################## Ping
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
 
-################################## Clear
-@commands.command(aliases=['c', 'clr'])
-async def clear(ctx, amount=2):
-    await ctx.channel.purge(limit=amount)
+    ################################## Clear
+    @commands.command(aliases=['c', 'clr'])
+    async def clear(ctx, amount=2):
+        await ctx.channel.purge(limit=amount)
 
 def setup(client):
     client.add_cog(Status(client))
